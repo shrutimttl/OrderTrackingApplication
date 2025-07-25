39716201f3c8aaa69f1084c72fe8e890d557ea31
shrutimttl <64834883+shrutimttl@users.noreply.github.com>
2025-07-25 20:23:24 +0530

    created routes to post and get

--- order-tracking-ui/src/App.js
+++ order-tracking-ui/src/App.js
@@ -1,17 +1,27 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import OrderForm from "./components/OrderForm";
 import OrderList from "./components/OrderList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Order Tracking System</h1>
-      </header>
-      <OrderForm />
-      <OrderList />
-    </div>
+    <Router>
+      <div className="container mt-4">
+        <nav className="mb-4">
+          <Link to="/" className="btn btn-primary me-2">
+            Place Order
+          </Link>
+          <Link to="/orders" className="btn btn-secondary">
+            View Orders
+          </Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<OrderForm />} />
+          <Route path="/orders" element={<OrderList />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
